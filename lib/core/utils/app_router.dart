16c3d285862a11d767902bf12e235e5e3daf7266
rama-fassi <rama.fassi@gmail.com>
2024-03-04 02:04:44 +0300

    book details appBar

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,12 @@
 import 'package:go_router/go_router.dart';
+import 'package:my_bookly/features/home/presenatation/views/book_details_view.dart';
 import 'package:my_bookly/features/home/presenatation/views/home_view.dart';
 import 'package:my_bookly/features/splash/presentation/views/splash_view.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
+  static const kBookDetailsView = '/bookDetailsView';
+
   static final router = GoRouter(routes: [
     GoRoute(
       path: '/',
@@ -12,6 +15,10 @@ abstract class AppRouter {
     GoRoute(
       path: kHomeView,
       builder: (context, state) => const HomeView(),
+    ),
+    GoRoute(
+      path: kBookDetailsView,
+      builder: (context, state) => const BookDetailsView(),
     ),
   ]);
 }
