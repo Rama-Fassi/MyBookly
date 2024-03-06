582b062b960923c329258f77b0ba53576845c6a0
rama-fassi <rama.fassi@gmail.com>
2024-03-07 00:05:22 +0300

    errors folder & failuer file & repo method

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,7 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:my_bookly/core/errors/failuers.dart';
+import 'package:my_bookly/features/home/data/models/book_model/book_model.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure,List<BookModel>>> fetchFeaturedBooks();
 }
 
-// create an abstract class and define a methods , these methods tell us 
+
+// Future<Either<Failure,List<BookModel>>> (this is the type for the method)
+
+// Repo ? create an abstract class and define a methods , these methods tell us 
 //what can this featured do (what is featured methods , actions (that will be execute , implement)),
