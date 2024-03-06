e5d17dc3013666b60da55a42661407733d376fa9
rama-fassi <rama.fassi@gmail.com>
2024-03-06 23:47:47 +0300

    create home repo

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,7 @@
+abstract class HomeRepo {
+  fetchBestSellerBooks();
+  fetchFeaturedBooks();
+}
+
+// create an abstract class and define a methods , these methods tell us 
+//what can this featured do (what is featured methods , actions (that will be execute , implement)),
