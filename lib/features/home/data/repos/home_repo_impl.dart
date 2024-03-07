5b4f2292eccd18c65a19c1540d910a5d97bd4b51
rama-fassi <rama.fassi@gmail.com>
2024-03-07 23:47:26 +0300

    api service

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:my_bookly/core/errors/failuers.dart';
+import 'package:my_bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:my_bookly/features/home/data/repos/home_repo.dart';
+
+class HomeRepoImpl implements HomeRepo{
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+
+}
