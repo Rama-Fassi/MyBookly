463d0785281798c270852dd6b1271f995aa558b9
rama-fassi <rama.fassi@gmail.com>
2024-03-12 00:40:14 +0300

    cached network image pub

--- lib/features/home/presenatation/views/widgets/custom_book_item.dart
+++ lib/features/home/presenatation/views/widgets/custom_book_item.dart
@@ -1,6 +1,5 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-
-import '../../../../../core/utils/assests.dart';
 
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({super.key, this.borderRadius, required this.imageUrl});
@@ -8,18 +7,29 @@ class CustomBookImage extends StatelessWidget {
   final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.7 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: borderRadius ?? BorderRadius.circular(16),
-          //color: Colors.red,
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: NetworkImage(
-             imageUrl,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: CachedNetworkImage(
+          imageUrl: imageUrl,
+          fit: BoxFit.fill,
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+         /*  placeholder: (context, url) =>
+              const Center(child: CircularProgressIndicator()), */
+
+          /* Container(
+          decoration: BoxDecoration(
+            borderRadius: borderRadius ?? BorderRadius.circular(16),
+            //color: Colors.red,
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+               imageUrl,
+              ),
             ),
           ),
+        ), */
         ),
       ),
     );
